20800031a2d47cb6643bfe0f1f25067813642ab1
TrungTai-gif <taint1272@ut.edu.vn>
2024-11-13 01:43:16 +0700

    Delete KoiFengShui.BE/KoiFengShui.BE directory

--- KoiFengShui.BE/KoiFengShui.BE/Model/ColorDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace KoiFengShui.BE.Model
-{
-    public class ELement_ColorDTO
-    {
-        public string ElementId { get; set; } = null!;
-        public string ColorId { get; set; } = null!;
-        public double ColorPoint { get; set; }
-    }
-}
