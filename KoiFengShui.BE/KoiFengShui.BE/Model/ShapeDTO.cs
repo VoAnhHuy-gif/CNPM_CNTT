c5a65bf146f6e82b0ecfbf9664335140874e8e50
TrungTai-gif <taint1272@ut.edu.vn>
2024-11-13 02:23:24 +0700

    Delete KoiFengShui.BE directory

--- KoiFengShui.BE/KoiFengShui.BE/Model/ShapeDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace KoiFengShui.BE.Model
-{
-    public class ShapeDTO
-    {
-        public string ShapeId { get; set; } = null!;
-        public string Image { get; set; } = null!;
-    }
-}
