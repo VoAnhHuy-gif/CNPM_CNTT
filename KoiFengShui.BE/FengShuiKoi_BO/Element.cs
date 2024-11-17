f52a5485df20b302659b1c1ee0060d91160ee41d
VoAnhHuy <huyva6423@ut.edu.vn@gmail.com>
2024-11-18 00:29:26 +0700

    Element

--- KoiFengShui.BE/FengShuiKoi_BO/Element.cs
+++ KoiFengShui.BE/FengShuiKoi_BO/Element.cs
@@ -15,7 +15,7 @@ namespace FengShuiKoi_BO
         public string ElementId { get; set; } = null!;
         public string Mutualism { get; set; } = null!;
 
-        public virtual QuantityOfFish? QuantityOfFish { get; set; }
+        public virtual QuantityOfFish { get; set; }
         public virtual ICollection<Advertisement> Advertisements { get; set; }
         public virtual ICollection<ElementColor> ElementColors { get; set; }
         public virtual ICollection<PointOfShape> PointOfShapes { get; set; }
